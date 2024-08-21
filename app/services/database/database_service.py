1a4d2644d43f8194538a7c1f7037e67158aae772
hungnp <hung.nguyen@digime.asia>
2024-08-21 21:14:33 +0700

    Refactor details for database services.

--- app/services/database/database_service.py
+++ app/services/database/database_service.py
@@ -10,47 +10,28 @@ class Database_Service:
         self.camera_store: CameraStore = system_store.camera_store
         self.stop_event = stop_event
         self.sli_database = sli_database
-        self.db_cursor = None
         self.logger = system_store.logger
 
     def database_init(self):
-        # check and create DB
-        self.sli_database.check_db_exists_create() 
-        # check and create table
+        self.sli_database.create_database()
         self.sli_database.create_table()
-        # use table
-        self.db_cursor = self.sli_database.conn.cursor()
-        command = "use %s; " % self.sli_database.db_to_create
-        self.db_cursor.execute(command)
-    
+
     def run(self):
         self.database_init()
-        self.system_store.DataBaseState.set_state(4) # ready
+        self.system_store.DataBaseState.set_state(4)  # ready
 
-        # infinite loop add data to table
         while not self.stop_event.is_set():
             if not self.camera_store.is_img_file_db_empty():
                 image_data = self.camera_store.get_first_img_file_from_queue()
-                # check list img data system_store not empty
                 if image_data:
-                    self.logger.info(f"{__class__.__name__}:have data for database")
-                    statement = "INSERT INTO sli_info (deviceID, imgID, date, time, lat, lon, alt, numSat) VALUES  (%s,%s,%s,%s,%s,%s,%s,%s);"
-                    val = (
-                        image_data.deviceID, image_data.imgID, image_data.imgDate, \
-                        image_data.imgTime, image_data.lat, image_data.lon, image_data.alt, image_data.numSat
-                    )
-                    self.db_cursor.execute(statement, val)
-                    self.sli_database.conn.commit()
-                    self.logger.info(f"{__class__.__name__}:__" + statement)
+                    self.sli_database.add_item_to_database(image_data)
             time.sleep(self.system_store.THREAD_SLEEP_1US)
 
-        self.system_store.DataBaseState.set_state(5) # stop
-        self.db_cursor.close()
-        self.sli_database.conn.close()
+        self.system_store.DataBaseState.set_state(5)  # stop
 
 def database_service_worker(system_store: SystemStore, stop_event: Event, sli_database: MySliDatabase):
     try:
         database_service = Database_Service(system_store, stop_event, sli_database)
         database_service.run()
     except Exception as e:
-        system_store.logger.error(f'Error in data base server - {e}')
+        system_store.logger.error(f'Error in database service - {e}')
--- app/services/database/my_sql_database.py
+++ app/services/database/my_sql_database.py
@@ -1,58 +1,98 @@
 from __future__ import annotations
 
 import os
-import pymysql # type: ignore
+import pymysql
+from services.image.img_filedata import FileImageData # type: ignore
 
 class MySliDatabase:
     def __init__(self) -> None:
+        self.host = 'localhost'
+        self.user = 'hungnp'
+        self.password = '1'
+        self.db_name = 'slidb'
+        self.db_table_name = 'sli_info'
+        self.db_root_path = os.path.join(os.getcwd(), 'data/db/')
+
         try:
-            # TODO - need to define this info as a config file or arguments
-            self.mariadb_root = os.path.join(os.getcwd(),'data/db/')
-            self.db_to_create = 'slidb'
-            self.conn = pymysql.connect(host='localhost',  user='hungnp', passwd='1',  cursorclass=pymysql.cursors.DictCursor)
+            self.conn = pymysql.connect(host=self.host, user=self.user, passwd=self.password, cursorclass=pymysql.cursors.DictCursor)
             print('Connection successful!!')
-
         except Exception as e:
-            print('Can not connect to %s' % self.db_to_create)
+            print(f'Can not connect to {self.db_name}')
             print(e)
+            raise e 
 
-    def __del__(self):
-        return
+    def __enter__(self):
+        return self
 
-    # Check if db exists, if not create
-    def check_db_exists_create(self):
-        dbfile = os.path.join(self.mariadb_root, self.db_to_create)
-        if not os.path.exists(dbfile):
-            try:
-                cur = self.conn.cursor()
-                command = "CREATE DATABASE IF NOT EXISTS %s;" % self.db_to_create
-                print(command)
-                cur.execute(command)
-                print("Database created!", self.db_to_create)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
 
-            except Exception as e:
-                print("Database creating failed!", self.db_to_create)
-                print(e)
-        else:
-            print("Database already exists: %s" % self.db_to_create)
-            
-    # create table
+    def create_database(self):
+        with self.conn.cursor() as cur:
+            cur.execute("SHOW DATABASES LIKE %s", (self.db_name,))
+            if cur.fetchone():
+                print(f"Database {self.db_name} already exists")
+            else:
+                cur.execute(f"CREATE DATABASE {self.db_name}")
+                print(f"Database {self.db_name} created successfully")
+
     def create_table(self):
-        cur = self.conn.cursor()
-        command = "use %s; " % self.db_to_create
-        cur.execute(command)
-        create_table = ("""
-                    CREATE TABLE IF NOT EXISTS sli_info (
-                    deviceID varchar(255),
-                    imgID varchar(255),
-                    date varchar(255),
-                    time varchar(255),
-                    lat varchar(255),
-                    lon varchar(255),
-                    alt varchar(255),
-                    numSat varchar(255),
-                    PRIMARY KEY (imgID));
-                    """)
-        print("create table")
-        cur.execute(create_table)
-        print(command)
+        with self.conn.cursor() as cur:
+            cur.execute(f"USE {self.db_name}")
+            cur.execute(f"SHOW TABLES LIKE '{self.db_table_name}'")
+            if cur.fetchone():
+                print(f"Table {self.db_table_name} already exists")
+            else:
+                create_table_sql = f"""
+                CREATE TABLE {self.db_table_name} (
+                deviceID varchar(255),
+                imgID varchar(255),
+                date varchar(255),
+                time varchar(255),
+                lat varchar(255),
+                lon varchar(255),
+                alt varchar(255),
+                numSat varchar(255),
+                PRIMARY KEY (imgID)
+                )
+                """
+                cur.execute(create_table_sql)
+                print(f"Table {self.db_table_name} created successfully")
+
+    def add_item_to_database(self, image_data: FileImageData):
+        statement = f"INSERT INTO {self.db_table_name} (deviceID, imgID, date, time, lat, lon, alt, numSat) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);"
+        val = (
+            image_data.deviceID, image_data.imgID, image_data.imgDate,
+            image_data.imgTime, image_data.lat, image_data.lon, image_data.alt, image_data.numSat
+        )
+        with self.conn.cursor() as cursor:
+            cursor.execute(statement, val)
+            self.conn.commit()
+
+    def row_to_img_data(self, row):
+        return FileImageData(
+            deviceID=row['deviceID'],
+            imgID=row['imgID'],
+            imgDate=row['date'],
+            imgTime=row['time'],
+            lat=row['lat'],
+            lon=row['lon'],
+            alt=row['alt'],
+            numSat=row['numSat']
+        )
+
+    def get_all_items_as_img_data(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {self.db_table_name}")
+            for row in cursor.fetchall():
+                yield self.row_to_img_data(row)
+
+    def remove_item(self, img_id):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"DELETE FROM {self.db_table_name} WHERE imgID = %s", (img_id,))
+            self.conn.commit()
+
+    def remove_all_items(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"TRUNCATE TABLE {self.db_table_name}")
+            self.conn.commit()
